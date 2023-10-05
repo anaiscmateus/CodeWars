659060c9b1b426acc6d3410272f6c8d71b7da18a
Anais Mateus <75923327+anaiscmateus@users.noreply.github.com>
2023-10-05 15:56:38 -0400

    daily codewars challenge

---
+++ Week 6/challenge04.js
@@ -0,0 +1,10 @@
+// Create a function that takes an integer as an argument and returns "Even" for even numbers or "Odd" for odd numbers.
+
+function evenOrOdd(number) {
+    //check to see if number is even
+    if (number % 2 === 0) {
+    return 'Number is even'
+    } else {
+    return 'Number is odd'
+    }
+}
