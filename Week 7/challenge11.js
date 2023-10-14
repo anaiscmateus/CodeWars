97cf62fff8f92385cc51523e5427c5b184c48e42
Anais Mateus <75923327+anaiscmateus@users.noreply.github.com>
2023-10-13 20:24:18 -0400

    adding 8KYU to file

--- Week 7/challenge11.js
+++ Week 7/challenge11.js
@@ -1,4 +1,4 @@
-/* You will be given an array a and a value x. All you need to do is check whether the provided array contains the value.
+/* 8KYU - You will be given an array a and a value x. All you need to do is check whether the provided array contains the value.
 
 Array can contain numbers or strings. X can be either.
 
