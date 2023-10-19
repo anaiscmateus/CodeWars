8ca9e9de75c263c86d84fbbb6b451079f567b938
Anais Mateus <75923327+anaiscmateus@users.noreply.github.com>
2023-10-19 12:00:00 -0400

    daily cw

--- 8KYU/challenges.js
+++ 8KYU/challenges.js
@@ -628,3 +628,20 @@ function sumStr(a, b) {
 
 console.log(sumStr("4", "5"), "9");
 console.log(sumStr("34", "5"), "39");
+
+/* Write a function that checks if a given string (case insensitive) is a palindrome.
+
+A palindrome is a word, number, phrase, or other sequence of symbols that reads the same backwards as forwards, such as madam or racecar. */
+
+function isPalindrome(x) {
+  return x.toLowerCase().split("").reverse().join("") === x.toLowerCase();
+}
+
+console.log(isPalindrome("a"), true);
+console.log(isPalindrome("aba"), true);
+console.log(isPalindrome("Abba"), true);
+console.log(isPalindrome("hello"), false);
+console.log(isPalindrome("Bob"), true);
+console.log(isPalindrome("Madam"), true);
+console.log(isPalindrome("AbBa"), true);
+console.log(isPalindrome(""), true);
