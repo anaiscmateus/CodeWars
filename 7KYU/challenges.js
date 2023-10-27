367fd234c46e117b3e681e6c45194ad7035d98bb
Anais Mateus <75923327+anaiscmateus@users.noreply.github.com>
2023-10-26 21:39:24 -0400

    daily cw challenge

--- 7KYU/challenges.js
+++ 7KYU/challenges.js
@@ -255,3 +255,36 @@ function SeriesSum(n)
     return sum.toFixed(2).toString()
     
 }
+
+/* Deoxyribonucleic acid (DNA) is a chemical found in the nucleus of cells and carries the "instructions" for the development and functioning of living organisms.
+
+If you want to know more: http://en.wikipedia.org/wiki/DNA
+
+In DNA strings, symbols "A" and "T" are complements of each other, as "C" and "G". Your function receives one side of the DNA (string, except for Haskell); you need to return the other complementary side. DNA strand is never empty or there is no DNA at all (again, except for Haskell). */
+
+// parameters - passing in a string of dna symbols
+// return - the complementary symbols for each letter passed in
+
+function DNAStrand(dna){
+    // your code here
+    // turn dna string into an array 
+    dnaArray = dna.split("")
+    // create a new array for the complementary string
+    let compArr = []
+    // loop through each letter
+    dnaArray.forEach(letter => {
+    // check the letter value and find it's complement and append to the array
+    if (letter === 'A') {
+        compArr.push('T')
+    } else if (letter === 'T') {
+        compArr.push('A')
+    } else if (letter === 'G') {
+        compArr.push('C')
+    } else if (letter === 'C') {
+        compArr.push('G')
+    }
+    })
+    // convert the array into a string
+    // return the complementary dna string
+    return compArr.join("")
+}
