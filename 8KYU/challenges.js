66fe496dee1894a8b16bd327f5a981d4d9e3d92b
Anais Mateus <75923327+anaiscmateus@users.noreply.github.com>
2023-10-28 20:39:32 -0400

    daily cw challenge

--- 8KYU/challenges.js
+++ 8KYU/challenges.js
@@ -294,3 +294,25 @@ function grow(x){
 
     return product // return the product variable
 }
+
+/* There was a test in your class and you passed it. Congratulations!
+But you're an ambitious person. You want to know if you're better than the average student in your class.
+
+You receive an array with your peers' test scores. Now calculate the average and compare your score!
+
+Return True if you're better, else False!
+
+Note:
+Your points are not included in the array of your class's points. For calculating the average point you may add your point to the given array! */
+
+// what are the parameters? an array of class mates grades and an integer representing your grade
+// what do we want to return? return whether or not your grade if higher than the class avg (true or false)
+
+function betterThanAverage(classPoints, yourPoints) {
+    // Your code here
+    // get the average of the grades of your classmates
+    let sum = 0
+    classPoints.forEach(point => sum += point)
+    // compare the average to your score and return true or false
+    return yourPoints > sum / classPoints.length ? true : false
+}
