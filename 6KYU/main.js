9d7beaf5c02a03662c06d5898aa80a712d89472d
Anais Mateus <75923327+anaiscmateus@users.noreply.github.com>
2023-10-29 12:00:00 -0400

    daily cw

--- 6KYU/main.js
+++ 6KYU/main.js
@@ -367,18 +367,38 @@ Examples
 * Input: [2,2,1,2,1], output = [2,2,2,1] */
 
 function removeSmallest(numbers) {
-    if (numbers.length === 0) {
-      return [];
-    }
-  
-    const smallestNumberIndex = numbers.indexOf(Math.min(...numbers));
-    return [
-      ...numbers.slice(0, smallestNumberIndex),
-      ...numbers.slice(smallestNumberIndex + 1),
-    ];
+  if (numbers.length === 0) {
+    return [];
   }
-  
-  console.log(removeSmallest([1, 2, 3, 4, 5]), [2, 3, 4, 5]);
-  console.log(removeSmallest([5, 3, 2, 1, 4]), [5, 3, 2, 4]);
-  console.log(removeSmallest([2, 2, 1, 2, 1]), [2, 2, 2, 1]);
-  console.log(removeSmallest([]), []);
+
+  const smallestNumberIndex = numbers.indexOf(Math.min(...numbers));
+  return [
+    ...numbers.slice(0, smallestNumberIndex),
+    ...numbers.slice(smallestNumberIndex + 1),
+  ];
+}
+
+console.log(removeSmallest([1, 2, 3, 4, 5]), [2, 3, 4, 5]);
+console.log(removeSmallest([5, 3, 2, 1, 4]), [5, 3, 2, 4]);
+console.log(removeSmallest([2, 2, 1, 2, 1]), [2, 2, 2, 1]);
+console.log(removeSmallest([]), []);
+
+// Write a function that takes an array of numbers (integers for the tests) and a target number. It should find two different items in the array that, when added together, give the target value. The indices of these items should then be returned in a tuple / list (depending on your language) like so: (index1, index2).
+
+// For the purposes of this kata, some tests may have multiple answers; any valid solutions will be accepted.
+
+// The input will always be valid (numbers will be an array of length 2 or greater, and all of the items will be numbers; target will always be the sum of two different items from that array).
+
+function twoSum(numbers, target) {
+  for (let i = 0; i < numbers.length; i++) {
+    for (let j = i + 1; j < numbers.length; j++) {
+      if (numbers[i] + numbers[j] === target) {
+        return [i, j];
+      }
+    }
+  }
+  return null;
+}
+
+console.log(twoSum([1, 2, 3], 4));
+console.log(twoSum([3, 2, 4], 6));
