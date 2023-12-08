2c979b158afc2aa2d177cc7df2bbadb369cd44f4
Anais Mateus <75923327+anaiscmateus@users.noreply.github.com>
2023-12-08 08:50:47 -0500

    daily cw challenge

--- 6KYU/main.js
+++ 6KYU/main.js
@@ -260,3 +260,25 @@ console.log(duplicateCount('abcde'))
 console.log(duplicateCount('aabbcde'))
 console.log(duplicateCount('aabBcde'))
 
+/* Take an array and remove every second element from the array. Always keep the first element and start removing with the next element.
+
+Example:
+["Keep", "Remove", "Keep", "Remove", "Keep", ...] --> ["Keep", "Keep", "Keep", ...]
+
+None of the arrays will be empty, so you don't have to worry about that! */
+
+// params are an array of numbers or words
+// return the indexes with an even number, but skip over 0
+
+function removeEveryOther(arr) {
+    return arr.map((e, i) => {
+      if (i === 0 || i % 2 === 0) {
+        return e
+      }
+    }).filter(e => e !== undefined)
+  }
+  
+  console.log(removeEveryOther(['Hello', 'Goodbye', 'Hello Again']),['Hello', 'Hello Again'])
+  console.log(removeEveryOther([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]),[1, 3, 5, 7, 9])
+  console.log(removeEveryOther([[1, 2]]), [[1, 2]])
+  console.log(removeEveryOther([['Goodbye'], {'Great': 'Job'}]),[['Goodbye']])
