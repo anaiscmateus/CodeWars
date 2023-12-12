a3fbe3a925096dd43b2f66c43a0fbf04054ca9cd
Anais Mateus <75923327+anaiscmateus@users.noreply.github.com>
2023-12-11 23:42:15 -0500

    daily cw

--- 8KYU/challenges.js
+++ 8KYU/challenges.js
@@ -515,3 +515,11 @@ function digitize(number) {
 function stringToArray(string){
     return string.split(" ")
 }
+
+/* Make a function that will return a greeting statement that uses an input; your program should return, "Hello, <name> how are you doing today?".
+
+[Make sure you type the exact thing I wrote or the program may not execute properly] */
+
+function greet(name){
+    return `Hello, ${name} how are you doing today?`;
+  }
