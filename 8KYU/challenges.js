b7264e64a36074c0b3847fb35474ed7c47906d7b
Anais Mateus <75923327+anaiscmateus@users.noreply.github.com>
2024-01-07 12:00:00 -0500

    daily cw

--- 8KYU/challenges.js
+++ 8KYU/challenges.js
@@ -670,3 +670,16 @@ console.log(abbrevName("Patrick Feenan"), "P.F");
 console.log(abbrevName("Evan Cole"), "E.C");
 console.log(abbrevName("P Favuzzi"), "P.F");
 console.log(abbrevName("David Mendieta"), "D.M");
+
+/* Write a function that removes the spaces from the string, then return the resultant string. */
+
+function noSpace(x) {
+  return x.split(" ").join("");
+}
+
+console.log(noSpace("8 j 8   mBliB8g  imjB8B8  jl  B"), "8j8mBliB8gimjB8B8jlB");
+console.log(
+  noSpace("8 8 Bi fk8h B 8 BB8B B B  B888 c hl8 BhB fd"),
+  "88Bifk8hB8BB8BBBB888chl8BhBfd"
+);
+console.log(noSpace("8aaaaa dddd r     "), "8aaaaaddddr");
