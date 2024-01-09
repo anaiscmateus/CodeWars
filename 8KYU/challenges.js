c6de2568e89c11e92ee5e230766dfed6034053b5
Anais Mateus <75923327+anaiscmateus@users.noreply.github.com>
2024-01-09 12:00:00 -0500

    daily cw

--- 8KYU/challenges.js
+++ 8KYU/challenges.js
@@ -691,3 +691,9 @@ class Kata {
     return length * width * height
   }
 }
+
+// Write a function that takes an array of numbers and returns the sum of the numbers. The numbers can be negative or non-integer. If the array does not contain any numbers then you should return 0.
+
+function sum(numbers) {
+  return numbers.reduce((a, b) => a + b, 0);
+}
