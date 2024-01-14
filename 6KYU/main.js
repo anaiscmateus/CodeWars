d808df27e2f286145644279d8861836cd71d7921
Anais Mateus <75923327+anaiscmateus@users.noreply.github.com>
2024-01-14 12:00:00 -0500

    daily cw

--- 6KYU/main.js
+++ 6KYU/main.js
@@ -402,3 +402,13 @@ function twoSum(numbers, target) {
 
 console.log(twoSum([1, 2, 3], 4));
 console.log(twoSum([3, 2, 4], 6));
+
+// The main idea is to count all the occurring characters in a string. If you have a string like aba, then the result should be {'a': 2, 'b': 1}.
+
+// What if the string is empty? Then the result should be empty object literal, {}.
+
+function count(string) {
+  return string.split("").reduce((a,b) => {a[b] = a[b] ? a[b] + 1 : 1; return a}, {})
+}
+
+console.log(count("aba"));
