2b6358199d76eb61cf095b7d5b7f0bf03bae304c
Anais Mateus <75923327+anaiscmateus@users.noreply.github.com>
2024-01-16 13:21:09 -0500

    daily cw

--- 7KYU/challenges.js
+++ 7KYU/challenges.js
@@ -348,3 +348,14 @@ function getMiddle(word) {
     
     return splitWord.length % 2 === 0 ? splitWord[middleIndex - 1] + splitWord[middleIndex] : splitWord[middleIndex]
 }
+
+// Implement a function that adds two numbers together and returns their sum in binary. The conversion can be done before, or after the addition.
+
+// The binary number returned should be a string.
+
+// Examples:(Input1, Input2 --> Output (explanation)))
+
+function addBinary(a,b) {
+    return (a + b).toString(2)
+}
+  
